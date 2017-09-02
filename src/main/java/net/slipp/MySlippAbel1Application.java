30848b851788699f9a220578ae2aad34d72f6976
abel <esung1129@gmail.com>
2017-09-02 16:20:12 +0900

    1-1. Spring Boot로 로컬 개발 환경 세팅
    - STS에서 Spring Boot 프로젝트를 생성
    - "Hello World"를 출력하는 HTML 페이지 생성
    - Spring Boot devtools와 chrome live reload extension을 활용한 개발 과정

---
+++ src/main/java/net/slipp/MySlippAbel1Application.java
@@ -0,0 +1,12 @@
+package net.slipp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySlippAbel1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySlippAbel1Application.class, args);
+	}
+}
