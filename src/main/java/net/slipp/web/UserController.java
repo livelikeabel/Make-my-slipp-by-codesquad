ea18be90bc252b035e55a453478aa659e410839c
abel <esung1129@gmail.com>
2017-09-05 17:09:07 +0900

    3-4. 개인정보 수정 기능 구현 1
    웹 애플리케이션 개발의 기본 흐름을 이해할 때 가장 복잡한 과정 중의 하나가 데이터베이스에 추가된 정보를 수정하는 것이다.
    회원가입한 사용자의 정보를 수정하는 기능을 1단계와 2단계로 나누어 진행한다.

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,6 +18,11 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@GetMapping("/form")//url에 입력하는 내용
+	public String form() {
+		return "/user/form"; //반환되는 파일 주소
+	}
+	
 	@PostMapping("")
 	public String create(User user) {
 		System.out.println("user : " + user);
@@ -28,5 +34,12 @@ public class UserController {
 	public String list(Model model) {
 		model.addAttribute("users", userRepository.findAll());
 		return "/user/list";
+	}
+	
+	@GetMapping("/{id}/form")
+	public String updateForm(@PathVariable Long id, Model model) {
+		User user = userRepository.findOne(id);
+		model.addAttribute("user", user);
+		return "/user/updateForm";
 	}
 }
