06783a83fbfcc6e293f73e80ff61111fbfa91b24
abel <esung1129@gmail.com>
2017-09-05 19:42:37 +0900

    3-5. 개인정보 수정 기능 구현 2
    -웹 애플리케이션 개발의 기본 흐름을 이해할 때 가장 복잡한 과정 중의 하나가 데이터베이스에 추가된 정보를 수정하는 것이다.
    -회원가입한 사용자의 정보를 수정하는 기능을 1단계와 2단계로 나누어 진행한다.
    - 수정기능 구현 잘 안됨 ㅠㅠㅠㅠ putmapping하는데 있어서 문제 있는듯...

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import net.slipp.domain.User;
@@ -42,4 +43,13 @@ public class UserController {
 		model.addAttribute("user", user);
 		return "/user/updateForm";
 	}
+	
+	@PutMapping("/{id}")
+	public String update(@PathVariable Long id, User newUser) {
+		User user = userRepository.findOne(id);
+		user.udate(newUser);
+		userRepository.save(user);
+		return "redirect:/users";
+	}
+	
 }
